5d5e43d7dfe494fdc7dd1ad8a6767cfbb57af4b0
Freddy Lopez <freddlopez01@gmail.com>
2026-05-05 13:20:57 -0700

    removed unnesscesary package

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { reactRouter } from "@react-router/dev/vite";
 import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   base: "/ciao/",   // ← must match your repo name exactly
