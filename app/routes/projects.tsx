dadb4c55f433d7b2c79635610bba977457532927
Freddy Lopez <freddlopez01@gmail.com>
2026-05-05 15:46:38 -0700

    testing

--- app/routes/projects.tsx
+++ app/routes/projects.tsx
@@ -1,4 +1,4 @@
-import { ComingSoon } from "../components/ComingSoon";
+import { ComingSoon } from "../components/ComingSoon.tsx";
 export default function Projects() {
   return <ComingSoon page="Projects" />;
 }
