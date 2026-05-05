0aca933286e312184372410db42813495e0c290b
Freddy Lopez <freddlopez01@gmail.com>
2026-05-05 13:14:50 -0700

    forgot to update name of repo

--- react-router.config.ts
+++ react-router.config.ts
@@ -2,7 +2,7 @@ import { reactRouter } from "@react-router/dev/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/your-repo-name/",   // ← must match your GitHub repo name exactly
+  base: "/ciao/",   // ← must match your GitHub repo name exactly
   plugins: [
     reactRouter({
       ssr: false,             // ← disables SSR, outputs pure static files
