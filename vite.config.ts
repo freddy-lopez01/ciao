99428b1434656181bfbd97665eb8ad03e245dd4f
Freddy Lopez <freddlopez01@gmail.com>
2026-05-05 14:40:49 -0700

    changes to reflect local or dev changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 
-export default defineConfig({
-  plugins: [tailwindcss(), reactRouter()],
-  resolve: {
-    tsconfigPaths: true,
-  },
-});
+export default defineConfig(({ mode }) => ({
+  base: mode === "production" ? "/ciao/" : "/",
+  plugins: [
+	tailwindcss(),
+    reactRouter(),
+  ],
+}));
